8af4a34b59e030ff75bc9bc39adf64f5fdd41488
Carlos Kimura <carloskimura@Carloss-MacBook-Pro.local>
2023-01-29 14:26:08 -0300

    adding multiple animation stages

--- Spinner/SpinnerView.swift
+++ Spinner/SpinnerView.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 struct SpinnerView: View {
     @State var currentIndex: Int?
+    @State var completed = false
     
     struct Leaf: View {
         let rotation: Angle
         let isCurrent: Bool
+        let isCompleting: Bool
         
         var body: some View {
             Capsule()
                 .stroke(isCurrent ? Color.white : Color.gray, lineWidth: 8)
-                .frame(width: 20, height: 50)
+                .frame(width: 20, height: isCompleting ? 20 : 50)
                 .offset(x: isCurrent ? 10 : 0, y: isCurrent ? 40 : 70)
                 .scaleEffect(isCurrent ? 0.5 : 1.0)
-                .rotationEffect(rotation)
-                .animation(.easeInOut(duration: 1.5), value: isCurrent)
+                .rotationEffect(isCompleting ? .zero : rotation)
+                .animation(.easeInOut(duration: 1.5), value: isCompleting)
         }
     }
     
@@ -31,7 +33,7 @@ struct SpinnerView: View {
         VStack {
             ZStack {
                 ForEach(0..<leavesCount) { index in
-                    Leaf(rotation: Angle(degrees: (Double(index) / Double(self.leavesCount)) * 360.0), isCurrent: index == self.currentIndex)
+                    Leaf(rotation: Angle(degrees: (Double(index) / Double(self.leavesCount)) * 360.0), isCurrent: index == self.currentIndex, isCompleting: self.completed)
                 }
             }
             .onAppear(perform: animate)
@@ -39,11 +41,19 @@ struct SpinnerView: View {
     }
     
     func animate() {
+        var iteration = 0
         Timer.scheduledTimer(withTimeInterval: 0.15, repeats: true) { timer in
             if let current = self.currentIndex {
                 self.currentIndex = (current + 1) % self.leavesCount
             } else {
                 self.currentIndex = 0
+            }
+            
+            iteration += 1
+            if iteration == 60 {
+                timer.invalidate()
+                self.completed = true
+                self.currentIndex = nil
             }
         }
     }
