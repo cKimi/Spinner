b2e951ef8155247181959a91477785072fde2f77
Carlos Kimura <carloskimura@Carloss-MacBook-Pro.local>
2023-01-29 13:50:16 -0300

    initial project files

---
+++ Spinner/SpinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinnerApp.swift
+//  Spinner
+//
+//  Created by Carlos Kimura on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SpinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
