5fa9155d36749818ad19f97ad5b761dc81c0ed20
Carlos Kimura <carloskimura@Carloss-MacBook-Pro.local>
2023-01-29 14:17:25 -0300

    first part of animation finished

--- Spinner/SpinnerView.swift
+++ Spinner/SpinnerView.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct SpinnerView: View {
+    @State var currentIndex: Int?
+    
     struct Leaf: View {
+        let rotation: Angle
+        let isCurrent: Bool
+        
         var body: some View {
             Capsule()
-                .stroke(Color.gray, lineWidth: 8)
+                .stroke(isCurrent ? Color.white : Color.gray, lineWidth: 8)
                 .frame(width: 20, height: 50)
+                .offset(x: isCurrent ? 10 : 0, y: isCurrent ? 40 : 70)
+                .scaleEffect(isCurrent ? 0.5 : 1.0)
+                .rotationEffect(rotation)
+                .animation(.easeInOut(duration: 1.5), value: isCurrent)
         }
     }
     
@@ -21,14 +30,22 @@ struct SpinnerView: View {
     var body: some View {
         VStack {
             ZStack {
-                Leaf()
+                ForEach(0..<leavesCount) { index in
+                    Leaf(rotation: Angle(degrees: (Double(index) / Double(self.leavesCount)) * 360.0), isCurrent: index == self.currentIndex)
+                }
             }
             .onAppear(perform: animate)
         }
     }
     
     func animate() {
-        
+        Timer.scheduledTimer(withTimeInterval: 0.15, repeats: true) { timer in
+            if let current = self.currentIndex {
+                self.currentIndex = (current + 1) % self.leavesCount
+            } else {
+                self.currentIndex = 0
+            }
+        }
     }
 }
 
